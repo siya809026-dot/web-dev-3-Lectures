4ef03e2066b0a059b3741983239f3f251787dc1f
Siya <siya809026@gmail.com>
2026-08-07 11:38:28 +0530

    first commit

---
+++ Lecture1/Script.js
@@ -0,0 +1,14 @@
+// const math = require('./math.js');
+// console.log(math.add(5,6));
+// console.log(math.subtract(5,6));
+
+
+
+// console.log("hello")
+// console.log(os.cpus())
+
+import {add,subtract} from './math.js';
+
+console.log(add(5,3));
+console.log(subtract(5,7));
+ 
