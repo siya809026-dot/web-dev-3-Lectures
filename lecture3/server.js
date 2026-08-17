317e39d1fc87d329d412133ec8ba50c5c1701653
Siya <siya809026@gmail.com>
2026-08-17 14:38:57 +0530

    post req

--- lecture3/server.js
+++ lecture3/server.js
@@ -1,5 +1,12 @@
 const http = require("http");
 
+const user=[
+    {id:101,name:"siya",email:"siya@gmail.com"},
+    {id:102,name:"riya",email:"riya@gmail.com"},
+    {id:103,name:"priya",email:"priya@gmail.com"},
+    {id:104,name:"diya",email:"diya@gmail.com"},
+]
+
 const server =  http.createServer((req,res)=>{
    // console.log(req.url);
    // console.log(req.headers);
@@ -16,13 +23,28 @@ if(req.url=="/"&& req.method=="GET"){
     res.writeHead(200,{"content-type":"text/html"});
     res.write("<h1>welcome to contactpage<h1>");
     res.end();
+}else if(req.url=="/user"&& req.method=="GET"){
+    res.writeHead(200,{"content-type":"application/json"});
+    res.write(JSON.stringify(user));
+    res.end();
+}else if(req.url=="/user"&& req.method=="POST"){
+    let body=""
+    req.on("data",(chunk)=>{
+        body+=chunk;
+    });
+    req.on("end",()=>{
+        const users = JSON.parse(body);
+        user.push(users);
+        res.writeHead(200,{"content-type":"application/json"});
+        res.write(JSON.stringify({"success":true,"message":"users created successfully"}));
+        res.end();                    
+    })    
 }else{
-    res.writeHead(200,{"Content-type":"text/html"});
+    res.writeHead(404,{"Content-type":"text/html"});
     res.write("<h1>404 page notfound<h1>");
     res.write("page not found");
 }   
-res.end()     
 })
 server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+    console.log("Server running on http://localhost:3000"); 
 });
